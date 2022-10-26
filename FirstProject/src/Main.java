096c5232ca928ab919c3402e569f19e35bb44e94
VarvaraMatveeva <forestknight567@outlook.com>
2022-10-26 20:32:29 +0300

    Commit test 2

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -5,20 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год:");
-        int year = scanner.nextInt();
 
+        int year = scanner.nextInt();
         if (isLeapYear(year)) {
-            System.out.println("Праздник будет " + "13.09." + year);
+            System.out.println("The celebration is on " + "12.09." + year);
         } else {
-            System.out.println("Праздник будет " + "12.09." + year);
+            System.out.println("The celebration is on " + "13.09." + year);
         }
     }
 
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0 && year % 400 != 0) {
-            return true;
-        } else if (year % 400 == 0 || year % 4 == 0) {
-        }
-        return false;
+        return !((year % 100 == 0 && year % 400 != 0) || year % 4 != 0);
     }
 }
